3a4dc4e914dfd99e7ef683757530a0f4825abf4f
Siang Lim <siang@alumni.ubc.ca>
2016-07-10 10:48:01 -0700

    'fuck'

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -251,8 +251,8 @@ Devise.setup do |config|
   # Add a new OmniAuth provider. Check the wiki for more information on setting
   # up on your models and hooks.
   # config.omniauth :github, 'APP_ID', 'APP_SECRET', scope: 'user,public_repo'
-  # config.omniauth :facebook, '1753534421570626', '3cac148394796ef2b4d2ecc8f298caaa'
-  config.omniauth :facebook, '1571425196486204', '66000998ef502188040359d512eb70e7'
+  config.omniauth :facebook, '1753534421570626', '3cac148394796ef2b4d2ecc8f298caaa'
+  # config.omniauth :facebook, '1571425196486204', '66000998ef502188040359d512eb70e7'
 
   # idgaf
 
