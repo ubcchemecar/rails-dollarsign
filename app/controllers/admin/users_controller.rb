b38b9fe1f5447c2847b58225051904e01f80155a
Siang Lim <siang@alumni.ubc.ca>
2016-07-12 23:16:37 -0700

    'permits'

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -51,7 +51,7 @@ class Admin::UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email, :approved)
+      params.require(:user).permit(:name, :email, :approved, :team)
     end
 
 end
